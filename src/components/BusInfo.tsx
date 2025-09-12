61206f7057e755193635d322a2cd62942415b1d5
inwoo <dlsdn1108@gmail.com>
2025-09-12 11:14:41 +0900

    버스 정보 표시 수정

--- src/components/BusInfo.tsx
+++ src/components/BusInfo.tsx
@@ -7,35 +7,39 @@ export default function BusInfo({ buses }: { buses: Bus[] }) {
         {buses.map((bus, idx) => (
           <li key={idx} className="flex-grow px-4 py-2 rounded-xl bg-[#EAF5EC]">
             <p className="font-bold text-xl text-center">
-              {bus.eta === "곧 도착" || bus.eta === "출발 준비"
+              {bus.eta === "곧 도착" || bus.eta === "출발 대기"
                 ? bus.eta
                 : `${bus.eta}후`}
             </p>
 
-            {!(bus.eta === "곧 도착" || bus.eta === "출발 준비") && (
+            {!(bus.eta === "곧 도착" || bus.eta === "출발 대기") && (
               <p className="font-semibold mb-2 text-center text-xs">
                 [ {bus.position} ]
               </p>
             )}
 
-            <p className="text-gray-500">도착 시간: {bus.arrival_time}</p>
-            <p className="text-gray-400">
-              {bus.bus_no} ({" "}
-              <span
-                className={
-                  bus.crowd === "여유"
-                    ? "text-green-500 font-semibold"
-                    : bus.crowd === "보통"
-                    ? "text-yellow-500 font-semibold"
-                    : bus.crowd === "혼잡"
-                    ? "text-red-500 font-semibold"
-                    : "text-gray-500 font-semibold"
-                }
-              >
-                {bus.crowd}
-              </span>{" "}
-              )
-            </p>
+            {bus.eta !== "출발 대기" && (
+              <>
+                <p className="text-gray-500">도착 시간: {bus.arrival_time}</p>
+                <p className="text-gray-400">
+                  {bus.bus_no} ({" "}
+                  <span
+                    className={
+                      bus.crowd === "여유"
+                        ? "text-green-500 font-semibold"
+                        : bus.crowd === "보통"
+                        ? "text-yellow-500 font-semibold"
+                        : bus.crowd === "혼잡"
+                        ? "text-red-500 font-semibold"
+                        : "text-gray-500 font-semibold"
+                    }
+                  >
+                    {bus.crowd}{" "}
+                  </span>
+                  )
+                </p>
+              </>
+            )}
           </li>
         ))}
       </ul>
