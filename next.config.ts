8c244e2c65b5948e5134341dfcd50c283a22ba3d
inwoo <dlsdn1108@gmail.com>
2025-09-12 11:30:33 +0900

    pwa 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
